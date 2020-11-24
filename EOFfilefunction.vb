0523774868aa3e429a55bb084b8a5f840afbb5fb
kevasd <39683554+kevasd@users.noreply.github.com>
2020-11-24 13:15:41 +0000

    Create EOFfilefunction.vb

---
+++ EOFfilefunction.vb
@@ -0,0 +1,6 @@
+Dim NewLine As String 'creates a line buffer to hold lines
+FileOpen(1, "Test", OpenMode.Input) Do While Not EOF(1) 'eof flag is used in new architectures 16bit computers had to find size of the file a map perfectly
+NewLine = LineInput(1) 'reads the line from the line offset (this is speculative as I have not read the source file
+lstDisplay.Items.Add(NewLine) 'displays all of the characters on newline variable (adv.higher *you might see special character such as newline tab interrupt they have a specified hex value don't be scared)
+Loop ' checks the condition flag (adv.higher * a flag is set is the same as a global variable that is automatically added to your program when it's run on Linux Linking is ld.so to check if two things are equal u can use Bitewize and and not to check if not or u can take them away from each other and they schould equal zero
+FileClose()
